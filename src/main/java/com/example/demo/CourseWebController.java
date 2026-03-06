76285af0d871c448a3a10f7387eed0002102317d
gp2k25 <goshapeters23@gmail.com>
2026-03-06 15:02:36 +0000

    final code

--- src/main/java/com/example/demo/CourseWebController.java
+++ src/main/java/com/example/demo/CourseWebController.java
@@ -16,14 +16,20 @@ public class CourseWebController {
         this.repo = repo;
     }
 
-    // LIST: show all courses
+    // NEW: The "Middle-man" that transforms the form parameter into a path variable
+    @GetMapping("/search-redirect")
+    public String searchRedirect(@RequestParam String data) {
+        return "redirect:/courses/" + data;
+    }
+
+
     @GetMapping
     public String list(Model model) {
         model.addAttribute("courses", repo.findAll());
         return "courses/list";
     }
 
-    // CREATE FORM: show an empty form
+
     @GetMapping("/new")
     public String newForm(Model model) {
         model.addAttribute("course", new Course());
@@ -31,7 +37,7 @@ public class CourseWebController {
         return "courses/form";
     }
 
-    // CREATE SUBMIT: save the posted form
+
     @PostMapping
     public String create(@ModelAttribute("course") Course course) {
         repo.save(course);
@@ -48,12 +54,11 @@ public class CourseWebController {
         return "courses/form";
     }
 
+
     @GetMapping("/{data}")
     public String searchForm(@PathVariable String data, Model model) {
         List<Course> filteredCourses = repo.searchAllFields(data);
-
         model.addAttribute("courses", filteredCourses);
-
         return "courses/list";
     }
 
